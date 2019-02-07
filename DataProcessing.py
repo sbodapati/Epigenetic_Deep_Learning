ffe4e0def1d0035d9fd41ed5780d5998b7240234
Sunil Bodapati <sbodapati@Sunils-MacBook-Pro.local>
2019-02-07 10:24:31 -0800

    segmented the locations by gene in rank order, looking to append it to a larger X file now.

--- DataProcessing.py
+++ DataProcessing.py
@@ -1,10 +1,12 @@
 import numpy as np
 import pandas as pd
 import pickle
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 
-def readEpigeneticDataFiles():
+def ReadEpigeneticDataFiles():
     print('starting to read raw data files')
 
     # A list of the chromatin locations. Shape = 184665, 1
@@ -37,4 +39,65 @@ def readEpigeneticDataFiles():
 
     pickle.dump((data_df, gene_ms, index_names), open( './pickle/raw_data_files.p', "wb" ))
     print('finished creating raw data pickle files')
+
+def ReadGeneDistanceFile():
+    print('starting to read distance file')
+    distanceFile = pd.read_csv('./data/gene2regionDistances.txt', sep = '\t', header=0)
+    pickle.dump(distanceFile, open( './pickle/distance_file.p', "wb" ))
+    print('finished reading distance file')
+
+def ConvertGeneDistanceFile(data_df, distanceFile, gene_ms, numFeatures, maxDistance):
+
+    # print(data_df.head())
+    # l = data_df.index.values
+    # l = [i.split('_',2) for i in l]
+    # chrom = [i[0] for i in l]
+    # start = [i[1] for i in l]
+    # end = [i[2] for i in l]
+
+    geneList= gene_ms.index.values
+    distanceFile['naming_conv'] = distanceFile['chrom'] + '_' + distanceFile['regionStart'].map(str) + '_' + distanceFile['regionEnd'].map(str)
+    indexNamesX = np.arange(1,numFeatures+1,1)
+    indexNamesY = ['expression']
+    finalX = pd.DataFrame(index=indexNamesX)
+    finalY = pd.DataFrame(index=indexNamesY)
+
+    for i in range(1): #geneList
+        gene = geneList[i]
+        geneDistance= distanceFile[(distanceFile.loc[:, 'gene'] == gene) & (distanceFile.loc[:, 'gene2regionDistance']<maxDistance)]
+        if geneDistance.shape[0] > numFeatures:
+            geneDistance.sort_values(by='gene2regionDistance', inplace=True)
+            tempX = data_df[data_df.index.isin(geneDistance.iloc[0:numFeatures,:]['naming_conv'])].reset_index()
+            print(tempX.head())
+            
+            # print(gene_ms[gene_ms.index.isin([gene])].set_index(indexNamesY ))
+
+
+
+
+    # print(data_df.head())
+    # print(distanceFile.head())
+    # print(gene_ms.head())
+
+
+    # distanceHist = set()
+    # print(distanceFile.head())
+    #
+    # sns.distplot(distanceFile.groupby(['gene'])['ones'].sum())
+    # plt.show()
+    # for gene in geneList:
+    #     geneDistance= distanceFile[distanceFile.loc[:,'gene'] == gene]
+    #     distanceHist.add(geneDistance.shape[0])
+
+    # sns.distplot(distanceHist)
+    # plt.show()
+    #
+    # print(start[0:5])
+    # print(end[0:5])
+    # print(distanceFile.head())
+    # print(distanceFile[ (distanceFile.loc[:,'regionStart'] == int(start[0])) & (distanceFile.loc[:,'regionEnd'] == int(end[0]))])
+    # print(data_df.head())
+    # LOXL4 = distanceFile[distanceFile.loc[:,'gene'] == 'LOXL4']
+    # print(LOXL4.head())
+    # print(LOXL4.sort_index(by = 'gene2regionDistance'))
 
